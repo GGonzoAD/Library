6d130081072b99ee75edafaa271c47008444bc1b
GGonzoAD <158357205+GGonzoAD@users.noreply.github.com>
2025-05-27 15:49:42 -0700

    message

---
+++ Library.js
@@ -0,0 +1,20 @@
+
+const myLibrary = [];
+
+function Book(title,author,pages,read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.findBook = function() {
+    console.log(this.title)
+    console.log(this.author)
+    console.log(this.pages)
+    console.log(this.read)
+  }
+  // the constructor...
+}
+
+function addBookToLibrary() {
+  // take params, create a book then store it in the array
+}
