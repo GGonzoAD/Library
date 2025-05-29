f324f9c192c1fff06468cce3592c0b0e792d49b5
GGonzoAD <158357205+GGonzoAD@users.noreply.github.com>
2025-05-28 19:34:25 -0700

    message

--- Library.js
+++ Library.js
@@ -6,7 +6,7 @@ function Book(title,author,pages,read) {
   this.author = author;
   this.pages = pages;
   this.read = read;
-  this.findBook = function() {
+  this.bookInfo = function() {
     console.log(this.title)
     console.log(this.author)
     console.log(this.pages)
@@ -19,3 +19,10 @@ function addBookToLibrary() {
   // take params, create a book then store it in the array
   
 }
+const book1 = new Book('Behold Humanity!: May We Come In?','Ralts Bloodthrone','471','Read');
+const book2 = new Book('The Complete Works of H.P. Lovecraft', 'H.P. Lovecraft', '1112','Not Read');
+const book3 = new Book('The Shadow: The Golden Vulture', 'Lester Dent', '128','Not Read');
+
+book1.bookInfo();
+book2.bookInfo();
+book3.bookInfo();
