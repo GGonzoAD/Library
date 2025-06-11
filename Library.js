eae6c0e18f7645d699de763f30d817c37b75d110
GGonzoAD <158357205+GGonzoAD@users.noreply.github.com>
2025-06-11 12:24:36 -0700

    message

--- Library.js
+++ Library.js
@@ -5,26 +5,25 @@ const myLibrary = [  // My Array of Books
 ];
 
 
-function Book(title, author, pages, genre, publicationDate ,read) {  // Constructor of books 
+function Book(title, author, pages, genre, publicationDate ,read) {  //Constructor of books 
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
   this.genre = genre;
   this.publicationDate = publicationDate;
-  this.bookInfo = function() { ///Makes Books
-    console.log (this.title,'by',this.author,this.pages,'Pages','Genre:',this.genre,'Year Published:',this.publicationDate,'Status:',this.read)
-
-    return (this.title,'by',this.author,this.pages,'Pages','Genre',this.genre,'Year Published:',this.publicationDate,'Status:',this.read)
-
-  };
   // the constructor...
 }
+Book.prototype.bookInfo = function() {
+  console.log (this.title,'by',this.author,this.pages,'Pages','Genre:',this.genre,'Year Published:',this.publicationDate,'Status:',this.read)
+
+  return (this.title,'by',this.author,this.pages,'Pages','Genre',this.genre,'Year Published:',this.publicationDate,'Status:',this.read)
+};
 
 const book2 = new Book ('The Talented Mr Ripley','Patrica Highsmith','252','Noir','1955','Not Reading');
 book2.bookInfo();
 
-function addBookToLibrary() { // add book to library with the parameters taken in.
+function addBookToLibrary() { //add book to library with the parameters taken in.
   
 }
 
