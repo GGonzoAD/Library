36774811777ca543df721b606a83859413dbfcc8
GGonzoAD <158357205+GGonzoAD@users.noreply.github.com>
2025-07-20 12:21:10 -0700

    Fixed the issue with add a book buttons

--- Library.js
+++ Library.js
@@ -76,11 +76,11 @@ function displayBook(){ // Displays Book contents
 displayBook();
 
 const modal = document.querySelector('.modal');
-const openModal = document.querySelector(".open-button");
+const openModal = document.querySelectorAll('.open-button');
 const closeModal = document.querySelector('.close-button');
 
-openModal.addEventListener("click",() => {
- modal.showModal();
+openModal.forEach(open => {
+  open.addEventListener('click', () => modal.showModal());
 })
 
 closeModal.addEventListener("click",(e) => {
